a1f47b6263253dca575b7f151c7472e682ddf096
sidney Alceu Andrade Wanderley Junior <sidney.andrade@gmail.com>
2017-12-06 12:19:23 -0300

    Enviando Projeto

---
+++ app/src/main/java/br/com/example/sidney/agenda/FormularioHelper.java
@@ -0,0 +1,50 @@
+package br.com.example.sidney.agenda;
+
+import android.widget.EditText;
+import android.widget.RatingBar;
+
+import br.com.example.sidney.agenda.modelo.Aluno;
+
+/**
+ * Created by sidney on 01/12/17.
+ */
+
+public class FormularioHelper {
+
+    private final EditText campoNome;
+    private final EditText campoEndereco;
+    private final EditText campoTelefone;
+    private final EditText campoSite;
+    private final RatingBar campoNota;
+
+    private Aluno aluno;
+
+    public FormularioHelper(FormularioActivity activity){
+        campoNome = (EditText) activity.findViewById(R.id.formulario_nome);
+        campoEndereco = (EditText) activity.findViewById(R.id.formulario_endereco);
+        campoTelefone = (EditText) activity.findViewById(R.id.formulario_telefone);
+        campoSite = (EditText) activity.findViewById(R.id.formulario_site);
+        campoNota = (RatingBar) activity.findViewById(R.id.formulario_nota);
+        aluno = new Aluno();
+    }
+
+    public Aluno pegaAluno() {
+        //Aluno aluno = new Aluno();
+        aluno.setNome(campoNome.getText().toString());
+        aluno.setEndereco(campoEndereco.getText().toString());
+        aluno.setTelefone(campoTelefone.getText().toString());
+        aluno.setSite(campoSite.getText().toString());
+        aluno.setNota(Double.valueOf(campoNota.getProgress()));
+
+        return aluno;
+    }
+
+    public void preencheFormulario(Aluno aluno) {
+        campoNome.setText(aluno.getNome());
+        campoEndereco.setText(aluno.getEndereco());
+        campoTelefone.setText(aluno.getTelefone());
+        campoSite.setText(aluno.getSite());
+        campoNota.setProgress(aluno.getNota().intValue());
+        this.aluno = aluno;
+    }
+}
